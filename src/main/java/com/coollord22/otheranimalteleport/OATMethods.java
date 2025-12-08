ca6408bd6192d86a9cea8f3e6349c0f1f5ea90d8
CoolLord22 <sachet.sachdeva@icloud.com>
2025-12-07 20:00:50 -0500

    Optimize and delay teleport tasks
    Uses more optimal getScheduler().runTaskLater() formatting for bukkit runnables rather than the new BukkitRunnable() format (helps with code readability).
    
    Additionally delays the actual teleport by 3 more ticks to allow chunk loading to process.
    
    Optimizes handling of invulnerability as its a shared method by both leashed/tamed entity teleports.

--- src/main/java/com/coollord22/otheranimalteleport/OATMethods.java
+++ src/main/java/com/coollord22/otheranimalteleport/OATMethods.java
@@ -1,69 +1,58 @@
 package com.coollord22.otheranimalteleport;
 
 import com.coollord22.otheranimalteleport.assets.Verbosity;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class OATMethods {
-	public static void teleportLeashedEnt(Entity ent, Location to, Player p, OtherAnimalTeleport plugin) {
-		String entID = "[Ent-" + ent.getEntityId() + "] ";
+	public static void teleportLeashedEnt(LivingEntity entity, Location to, Player p, OtherAnimalTeleport plugin) {
+		String entID = "[Ent-" + entity.getEntityId() + "] ";
 
-		plugin.log.logInfo(entID + "Attempting to null the leash holder.", Verbosity.HIGHEST);
-		((LivingEntity) ent).setLeashHolder(null);
+		plugin.log.logInfo(entID + "Received leashed-entity teleport. Attempting to null current leash holder.", Verbosity.HIGHEST);
+		entity.setLeashHolder(null);
 
-		boolean invulnerable = ent.isInvulnerable();
-		
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				plugin.log.logInfo(entID + "Protecting entity with invulnerability and resistance.", Verbosity.HIGHEST);
-				ent.setInvulnerable(true);
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+			handleInvulnerability(entity, plugin);
 
-				plugin.log.logInfo(entID + "Teleporting entity " + ent.getType(), Verbosity.HIGH);
-				ent.teleport(to);
+			plugin.log.logInfo(entID + "Teleporting entity " + entity.getType(), Verbosity.HIGH);
+			if(!entity.teleport(to)) {
+				throw new RuntimeException("Unsuccessful entity teleport");
+			}
 
+			Bukkit.getScheduler().runTaskLater(plugin, () -> {
 				// Delay re-attaching of leash by 10 ticks to ensure entity pathfinding cant freeze
-				new BukkitRunnable() {
-					@Override
-					public void run() {
-						plugin.log.logInfo(entID + "Re-attaching leash holder as " + p.getName() + ".", Verbosity.HIGHEST);
-						((LivingEntity)ent).setLeashHolder(p);
-					}
-				}.runTaskLater(plugin, 10L);
-
-				undoInvulnerable(ent, invulnerable, plugin);
-			}
-		}.runTaskLater(plugin, 2L);
+				plugin.log.logInfo(entID + "Re-attaching leash holder as " + p.getName() + ".", Verbosity.HIGHEST);
+				entity.setLeashHolder(p);
+			}, 10L);
+		}, 5L);
 	}
 
-	public static void teleportEnt(Entity ent, Location to, Player p, OtherAnimalTeleport plugin) {
-		String entID = "[Ent-" + ent.getEntityId() + "] ";
+	public static void teleportTamedEnt(Entity entity, Location to, OtherAnimalTeleport plugin) {
+		String entID = "[Ent-" + entity.getEntityId() + "] ";
 
-		boolean invulnerable = ent.isInvulnerable();
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+			handleInvulnerability(entity, plugin);
 
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				plugin.log.logInfo(entID + "Protecting entity with invulnerability and resistance.", Verbosity.HIGHEST);
-				ent.setInvulnerable(true);
-
-				plugin.log.logInfo(entID + "Teleporting entity " + ent.getType(), Verbosity.HIGH);
-				ent.teleport(to);
-				undoInvulnerable(ent, invulnerable, plugin);
+			plugin.log.logInfo(entID + "Teleporting entity " + entity.getType(), Verbosity.HIGH);
+			if(!entity.teleport(to)) {
+				throw new RuntimeException("Unsuccessful entity teleport");
 			}
-		}.runTaskLater(plugin, 2L);
+		}, 5L);
 	}
 
-	private static void undoInvulnerable(Entity ent, boolean invulnerable, OtherAnimalTeleport plugin) {
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				plugin.log.logInfo("[Ent-" + ent.getEntityId() + "] Reverting invulnerability status.", Verbosity.HIGHEST);
-				ent.setInvulnerable(invulnerable);
-			}
-		}.runTaskLater(plugin, 30L);
+	private static void handleInvulnerability(Entity entity, OtherAnimalTeleport plugin) {
+		String entID = "[Ent-" + entity.getEntityId() + "] ";
+		boolean wasInvulnerable = entity.isInvulnerable();
+
+		plugin.log.logInfo(entID + "Protecting entity with invulnerability and resistance.", Verbosity.HIGHEST);
+		entity.setInvulnerable(true);
+
+		Bukkit.getScheduler().runTaskLater(plugin, () -> {
+			plugin.log.logInfo(entID + "Reverting invulnerability status.", Verbosity.HIGHEST);
+			entity.setInvulnerable(wasInvulnerable);
+		}, 30L);
 	}
 }
